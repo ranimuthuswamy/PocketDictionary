292b610070c04bea7c316a4d17434e2e7599359b
ranimuthuswamy <67144212+ranimuthuswamy@users.noreply.github.com>
2020-11-08 23:18:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View } from 'react-native';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import HomeScreen from './screens/HomeScreen';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <AppContainer/>
+      </View>
+    );
+  }
+}
+
+var AppNavigator = createSwitchNavigator({
+  Home:HomeScreen
+});
+
+const AppContainer = createAppContainer(AppNavigator);
---
+++ screens/HomeScreen.js
@@ -0,0 +1,163 @@
+import * as React from 'react';
+import { Text, TextInput, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Header } from 'react-native-elements';
+
+export default class HomeScreen extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            text: '',
+            isSearchPressed: false,
+            word: 'Loading...',
+            lexicalCategory: '',
+            examples: [],
+            defination: ''
+        }
+    }
+
+    getWord = (word) => {
+        var url = "https://whitehat-dictionary-glitch.me/?word=" + word;
+        return fetch(url)
+            .then((data) => {
+                return data.json()
+            })
+            .then((response) => {
+                var responseObject = JSON.parse(response);
+                var word = responseObject.word;
+                var lexicalCategory = responseObject.results[0].lexicalEntries[0].lexicalCategory.text;
+                var defination = responseObject.results[0].lexicalEntries[0].senses[0].definitions[0];
+
+                this.setState({
+                    "word": word.trim(),
+                    "lexicalCategory": lexicalCategory === undefined ? "" : lexicalCategory.trim(),
+                    "defination": defination === undefined ? "" : defination.trim()
+                });
+            })
+    }
+
+    render() {
+        return (
+            <View>
+                <Header backgroundColor={'purple'}
+                    centerComponent={{ text: "Pocket Dictionary", style: { fontSize: 20, color: '#fff' } }}
+                />
+                <View style={styles.inputBoxContainer}>
+                    <TextInput
+                        style={styles.inputBox}
+                        onChangeText={(text) => {
+                            this.setState({
+                                text: text,
+                                isSearchPressed: false,
+                                word: 'Loading...',
+                                lexicalCategory: '',
+                                examples: [],
+                                defination: ''
+                            });
+                        }}
+                        value={this.state.text}
+                    />
+                    <TouchableOpacity
+                        style={styles.searchButton}
+                        onPress={() => {
+                            this.setState({ isSearchPressed: true });
+                            this.getWord(this.state.text);
+                        }}
+                    >
+                        <Text style={styles.buttonText}>Search</Text>
+                    </TouchableOpacity>
+                    <View style={styles.outputBoxContainer}>
+                        <Text style={{ fontSize: 20 }}>
+                            {
+                                this.state.isSearchPressed && this.state.word === 'Loading...'
+                                    ? this.state.word
+                                    : ""
+                            }
+                        </Text>
+                        {
+                            this.state.word !== 'Loading...' ?
+                                (
+                                    <View style={{ justifyContent: 'center', marginLeft: 10 }}>
+                                        <View style={styles.detailsContainer}>
+                                            <Text style={styles.detailsTitle}>
+                                                Word:{" "}
+                                            </Text>
+                                            <Text style={{ fontSize: 18 }}>
+                                                {this.state.word}
+                                            </Text>
+                                        </View>
+                                        <View style={styles.detailsContainer}>
+                                            <Text style={styles.detailsTitle}>
+                                                Type:{" "}
+                                            </Text>
+                                            <Text style={{ fontSize: 18 }}>
+                                                {this.state.lexicalCategory}
+                                            </Text>
+                                        </View>
+                                        <View style={{ flexDirection: 'row', flexWrap: "wrap" }}>
+                                            <Text style={styles.detailsTitle}>
+                                                Definition:{" "}
+                                            </Text>
+                                            <Text style={{ fontSize: 18 }}>
+                                                {this.state.defination}
+                                            </Text>
+                                        </View>
+                                    </View>
+                                )
+                                : null
+                        }
+                    </View>
+                </View>
+            </View>
+        );
+    }
+
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    inputBoxContainer: {
+        flex: 0.3,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    inputBox: {
+        marginTop: 50,
+        width: '80%',
+        height: 55,
+        borderWidth: 4,
+        alignSelf: 'center',
+        textAlign: 'center',
+        fontSize: 25
+    },
+    searchButton: {
+        marginTop: 20,
+        width: '40%',
+        height: 50,
+        alignSelf: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 2,
+        borderRadius: 15,
+        backgroundColor: 'orange'
+    },
+    buttonText: {
+        fontSize: 30,
+        fontWeight: 'bold'
+    },
+    outputContainer:{
+        flex:0.7,
+        alignItems:'center'
+      },
+      detailsContainer:{
+        flexDirection:'row',
+        alignItems:'center'
+      },
+      detailsTitle:{
+        color:'orange',
+        fontSize:20,
+        fontWeight:'bold'
+      }
+});
